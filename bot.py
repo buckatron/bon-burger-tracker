389ec2db7ca7fcf99530d53e9f297829d185f0de
Buck Stevenson <47488933+buckatron@users.noreply.github.com>
2026-09-20 16:52:47 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,12 @@ from datetime import date
 from datetime import datetime, timedelta
 from urllib.request import Request, urlopen
 
-client = tweepy.Client(os.environ["API_KEY"],os.environ["API_SECRET"],os.environ["ACCESS_TOKEN"],os.environ["ACCESS_TOKEN_SECRET"])
-
+client = tweepy.Client(
+    consumer_key=os.environ["API_KEY"],
+    consumer_secret=os.environ["API_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
+)
 week = []
 days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 presentday = datetime.now()
